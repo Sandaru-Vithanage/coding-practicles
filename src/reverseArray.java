7ac4c62430379af48a16b476f72cae05899a00b0
Sandaru-Vithanage <sandaru.20210334@iit.ac.lk>
2025-02-03 20:36:14 +0530

    refined reverse array

--- src/reverseArray.java
+++ src/reverseArray.java
@@ -3,9 +3,17 @@ public class reverseArray {
         public void rotate(int[] nums, int k) {
             int n = nums.length;
             k = k % n;
+            System.out.println("Original array: " + java.util.Arrays.toString(nums));
+            System.out.println("Rotate steps (k): " + k);
+
             reverseString(nums, 0, n-k-1);
+            System.out.println("After reversing first part: " + java.util.Arrays.toString(nums));
+
             reverseString(nums, n-k, n-1);
+            System.out.println("After reversing second part: " + java.util.Arrays.toString(nums));
+
             reverseString(nums, 0, n-1);
+            System.out.println("After reversing entire array: " + java.util.Arrays.toString(nums));
         }
 
         public void reverseString(int nums[], int start, int end){
@@ -31,5 +39,12 @@ public class reverseArray {
                 nums[i] = rotated[i];
             }
         }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new reverseArray().new Solution();
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        solution.rotate(nums, k);
     }
 }
