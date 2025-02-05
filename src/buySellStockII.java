ccde8e15a4f2ba8b5fa6a7afbb0ce9cf3824f08c
Sandaru-Vithanage <sandaru.20210334@iit.ac.lk>
2025-02-05 22:02:50 +0530

    added another way to buysellStockII

--- src/buySellStockII.java
+++ src/buySellStockII.java
@@ -10,4 +10,19 @@ public class buySellStockII {
         return maxProfit;
     }
 
+    public int maxProfit2(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > 0) {
+                profit += prices[i] - buy;
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+
 }
