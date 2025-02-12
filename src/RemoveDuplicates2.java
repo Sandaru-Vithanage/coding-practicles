cd7aa6524a33284a6e13188533ce1838cb86f8b4
Sandaru-Vithanage <sandaru.20210334@iit.ac.lk>
2025-02-12 20:59:39 +0530

    modified remove duplicates module

--- src/RemoveDuplicates2.java
+++ src/RemoveDuplicates2.java
@@ -1,14 +1,33 @@
+/**
+ * This class provides a method to remove duplicates from a sorted array
+ * such that each element appears at most twice.
+ */
 public class RemoveDuplicates2 {
 
-        public int removeDuplicates(int[] nums) {
-            int counter = 2;
-            for(int i=2;i < nums.length; i++){
-                if(nums[i] != nums[counter-2]){
-                    nums[counter] = nums[i];
-                    counter++;
-                }
+    /**
+     * Removes duplicates from the sorted array such that each element appears at most twice.
+     *
+     * @param nums the input sorted array
+     * @return the length of the array after removing duplicates
+     */
+    public int removeDuplicates(int[] nums) {
+        // Initialize the counter to 2 since the first two elements are always allowed
+        int counter = 2;
+
+        // Iterate through the array starting from the third element
+        for (int i = 2; i < nums.length; i++) {
+            // If the current element is not equal to the element at position counter-2,
+            // it means it can be included in the result array
+            if (nums[i] != nums[counter - 2]) {
+                // Assign the current element to the position indicated by the counter
+                nums[counter] = nums[i];
+                // Increment the counter
+                counter++;
             }
-            return counter;
         }
+
+        // Return the length of the array after removing duplicates
+        return counter;
     }
+}
 
