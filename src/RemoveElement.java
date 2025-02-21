0a4d13ad8e8e35e43203e7233489a2b6f74d880d
Sandaru-Vithanage <sandaru.20210334@iit.ac.lk>
2025-02-21 23:02:07 +0530

    changed remove element

--- src/RemoveElement.java
+++ src/RemoveElement.java
@@ -1,7 +1,7 @@
 public class RemoveElement {
     class Solution {
         public int removeElement(int[] nums, int val) {
-            int k = 0; // Pointer for the next valid position
+            int k = 0;
             for (int i = 0; i < nums.length; i++) {
                 // If the current element is not equal to val, copy it to the k-th position
                 if (nums[i] != val) {
