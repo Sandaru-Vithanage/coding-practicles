0573917e4ee464d468f4c3c389e930fad7073651
Sandaru-Vithanage <sandaru.20210334@iit.ac.lk>
2025-02-24 22:46:58 +0530

    added extension

--- src/RemoveElement.java
+++ src/RemoveElement.java
@@ -3,7 +3,6 @@ public class RemoveElement {
         public int removeElement(int[] nums, int val) {
             int k = 0; // Pointer for the next valid position
             for (int i = 0; i < nums.length; i++) {
-                // If the current element is not equal to val, copy it to the k-th position
                 if (nums[i] != val) {
                     nums[k] = nums[i];
                     k++;
