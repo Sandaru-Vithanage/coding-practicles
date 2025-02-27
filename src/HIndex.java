2e398c644dbccdf5dca2f9ce16a0ee143ba6edf5
Sandaru-Vithanage <sandaru.20210334@iit.ac.lk>
2025-02-27 22:41:09 +0530

    minor changes

--- src/HIndex.java
+++ src/HIndex.java
@@ -1,2 +1,13 @@
 import java.util.Arrays;
 
+public class HIndex {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int len = citations.length;
+        int i = 0;
+        while (i < len && i < citations[len - i - 1]) {
+            i++;
+        }
+        return i;
+    }
+}
