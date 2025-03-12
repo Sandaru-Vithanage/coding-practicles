770cb3fe47c9d719e50be8011968d0be6d758212
Sandaru-Vithanage <sandaru.20210334@iit.ac.lk>
2025-03-12 23:53:45 +0530

    Changed Best Buy Stocks

--- src/BestBuyStocks.java
+++ src/BestBuyStocks.java
@@ -28,4 +28,21 @@ public class BestBuyStocks {
         }
         return bestDay;
     }
+
+    public int bestTime(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        int bestDay = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                if (prices[i] - minPrice > maxProfit) {
+                    maxProfit = prices[i] - minPrice;
+                    bestDay = i;
+                }
+            }
+        }
+        return bestDay;
+    }
 }
