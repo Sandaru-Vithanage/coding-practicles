ee4ce1d4a3e6745200da67887001e39061737fd2
Sandaru-Vithanage <sandaru.20210334@iit.ac.lk>
2025-03-13 23:48:40 +0530

    added factorial problem

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -14,3 +14,12 @@ public class FibonacciSeries {
         }
     }
 }
+
+class Factorial {
+    public static long factorial(long n) {
+        if (n == 1)
+            return 1;
+        else
+            return (n * factorial(n - 1));
+    }
+}
