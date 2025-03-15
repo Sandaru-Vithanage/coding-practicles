dbad9ffe0a4da8718ceca4c6fba50b426b50bf39
Sandaru-Vithanage <sandaru.20210334@iit.ac.lk>
2025-03-15 23:16:13 +0530

    added prime check

---
+++ src/primeCheck.java
@@ -0,0 +1,14 @@
+public class primeCheck {
+
+    public boolean primeChecker(int num){
+        if(num <= 1){
+            return false;
+        }
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
