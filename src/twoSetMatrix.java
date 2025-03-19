2b183b54b3c47b330e56d8f1134a8bd3891e0727
Sandaru-Vithanage <sandaru.20210334@iit.ac.lk>
2025-03-19 23:38:11 +0530

    minor fixes

--- src/twoSetMatrix.java
+++ src/twoSetMatrix.java
@@ -15,4 +15,16 @@ public class twoSetMatrix {
         }
         return matrix;
     }
+
+    public static void main(String[] args) {
+        twoSetMatrix twoSetMatrix = new twoSetMatrix();
+        int n = 3;
+        int[][] matrix = twoSetMatrix.twoSetMatrix(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
