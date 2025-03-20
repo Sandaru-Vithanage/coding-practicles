9177832acd4b3c9b7a4b0bfff8764a5fa7311928
Sandaru-Vithanage <sandaru.20210334@iit.ac.lk>
2025-03-20 23:03:57 +0530

    fixed changes in sum metrices problem

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -13,31 +13,31 @@ public class ThreeSum {
         return result;
     }
 
-//    public static void main(String[] args) {
-//        ThreeSum threeSum = new ThreeSum();
-//        int[][] matrix1 = {
-//                {1, 2, 3},
-//                {4, 5, 6},
-//                {7, 8, 9}
-//        };
-//        int[][] matrix2 = {
-//                {9, 8, 7},
-//                {6, 5, 4},
-//                {3, 2, 1}
-//        };
-//        int[][] matrix3 = {
-//                {1, 1, 1},
-//                {1, 1, 1},
-//                {1, 1, 1}
-//        };
-//
-//        int[][] result = threeSum.threeSum(matrix1, matrix2, matrix3);
-//
-//        for (int i = 0; i < result.length; i++) {
-//            for (int j = 0; j < result[i].length; j++) {
-//                System.out.print(result[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
+    public static void main(String[] args) {
+        ThreeSum threeSum = new ThreeSum();
+        int[][] matrix1 = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        int[][] matrix2 = {
+                {9, 8, 7},
+                {6, 5, 4},
+                {3, 2, 1}
+        };
+        int[][] matrix3 = {
+                {1, 1, 1},
+                {1, 1, 1},
+                {1, 1, 1}
+        };
 
+        int[][] result = threeSum.threeSum(matrix1, matrix2, matrix3);
+
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
