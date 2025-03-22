9eff00c143c1fa1bfa5b150480bc988f8e7df45b
Sandaru-Vithanage <sandaru.20210334@iit.ac.lk>
2025-03-22 22:55:51 +0530

    added metrix based changes

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -20,24 +20,7 @@ public class ThreeSum {
                 {4, 5, 6},
                 {7, 8, 9}
         };
-        int[][] matrix2 = {
-                {9, 8, 7},
-                {6, 5, 4},
-                {3, 2, 1}
-        };
-        int[][] matrix3 = {
-                {1, 1, 1},
-                {1, 1, 1},
-                {1, 1, 1}
-        };
-
-        int[][] result = threeSum.threeSum(matrix1, matrix2, matrix3);
-
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result[i].length; j++) {
-                System.out.print(result[i][j] + " ");
-            }
+        int[] result = {1,2,3,4};
             System.out.println();
         }
-    }
 }
