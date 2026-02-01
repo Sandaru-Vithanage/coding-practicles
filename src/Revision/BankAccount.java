ea44792d5d975384d30e1879020ded57a95a1e7c
Sandaru-Vithanage <sandaru.20210334@iit.ac.lk>
2026-02-01 20:03:48 +0530

    Add revisions

---
+++ src/Revision/BankAccount.java
@@ -0,0 +1,22 @@
+package Revision;
+
+public class BankAccount {
+    private String accountHolderName;
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountHolderName, String accountNumber, double initialBalance) {
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    synchronized public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount + ", New Balance: " + balance);
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+}
